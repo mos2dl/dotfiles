1d81399db64b5daf57667f342e4b51dbe859bd3c
Daniel Lindroth <daniel@compulartech.com>
2026-02-19 08:35:07 +0000

    feat: neovim config tweaks

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+-- return {
+--
+-- 	{ "catppuccin/nvim" },
+--
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "catppuccin",
+-- 		},
+-- 	},
+-- }
+
+-- return {
+-- 	{ "ellisonleao/gruvbox.nvim" },
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "gruvbox",
+-- 		},
+-- 	},
+-- }
+
+return {
+	{ "neanias/everforest-nvim" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "everforest",
+			background = "soft",
+		},
+	},
+}
